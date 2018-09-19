c9c8da2d7a31051ceeb346921459595bd52dddf0
corkili <corkili@163.com>
2018-09-19 16:20:56 +0800

    #3 增加EmailPO

---
+++ husky-entity/src/main/java/com/corkili/husky/email/Email.java
@@ -0,0 +1,74 @@
+package com.corkili.husky.email;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLDeleteAll;
+import org.hibernate.annotations.Where;
+import org.hibernate.annotations.WhereJoinTable;
+import org.hibernate.validator.constraints.Range;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import com.corkili.husky.common.State;
+import com.corkili.husky.user.UserPO;
+
+@Entity
+@Table(name = "t_email")
+@SQLDelete(sql = "update t_email set state = " + State.DELETED + " where id = ?")
+@SQLDeleteAll(sql = "update t_email set state = " + State.DELETED + " where id = ?")
+@Where(clause = "state != " + State.DELETED)
+@WhereJoinTable(clause = "state != " + State.DELETED)
+@Getter
+@Setter
+@ToString
+public class Email {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "create_time", updatable = false, nullable = false,
+            columnDefinition = "timestamp DEFAULT CURRENT_TIMESTAMP")
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date createTime;
+
+    @Column(name = "update_time", nullable = false,
+            columnDefinition = "timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date updateTime;
+
+    @Column(name = "state", nullable = false)
+    @Range(min = State.INIT, max = State.DELETED)
+    @NotNull
+    private int state;
+
+    @Column(name = "email_address", nullable = false)
+    @javax.validation.constraints.Email
+    @NotBlank
+    private String emailAddress;
+
+    @ManyToOne(cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "user_id")
+    @Fetch(FetchMode.JOIN)
+    private UserPO belongUser;
+}
