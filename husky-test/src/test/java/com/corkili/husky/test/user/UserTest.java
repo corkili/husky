c30f1b9b77fc09139519ba9d2f49e5a329fefb22
corkili <corkili@163.com>
2018-09-19 11:12:42 +0800

    #5 增加测试模块

---
+++ husky-test/src/test/java/com/corkili/husky/test/user/UserTest.java
@@ -0,0 +1,54 @@
+package com.corkili.husky.test.user;
+
+import java.util.Date;
+
+import javax.jws.soap.SOAPBinding.Use;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.corkili.husky.common.State;
+import com.corkili.husky.test.HuskyTest;
+import com.corkili.husky.user.UserPO;
+import com.corkili.husky.user.UserRepository;
+
+public class UserTest extends HuskyTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testInsert() {
+        UserPO userPO = new UserPO();
+        userPO.setUsername("test-user");
+        userPO.setPassword("passkey");
+        userPO.setCreateTime(new Date());
+        userPO.setUpdateTime(new Date());
+        userPO.setState(State.ACTIVE);
+        userPO = userRepository.save(userPO);
+        System.out.println(userPO);
+    }
+
+    @Test
+    public void testQuery() {
+        UserPO userPO = userRepository.findUserPOByUsername("test-user");
+        System.out.println(userPO);
+    }
+
+    @Test
+    public void testUpdate() {
+        UserPO userPO = userRepository.findUserPOByUsername("test-user");
+        System.out.println(userPO);
+        userPO.setPassword("passkey1");
+        userPO = userRepository.save(userPO);
+        System.out.println(userPO);
+    }
+
+    @Test
+    public void testDelete() {
+        UserPO userPO = userRepository.findUserPOByUsername("test-user");
+        System.out.println(userPO);
+        userRepository.delete(userPO);
+    }
+
+}
