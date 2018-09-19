37cbeb864f7443b57b9d20f2dfd98f26b213b128
corkili <corkili@163.com>
2018-09-19 10:27:18 +0800

    #3 增加UserPO和AccountItemPO

---
+++ husky-entity/src/main/java/com/corkili/husky/common/State.java
@@ -0,0 +1,10 @@
+package com.corkili.husky.common;
+
+public final class State {
+
+    public static final int INIT = 0;
+    public static final int ACTIVE = 1;
+    public static final int INACTIVE = 2;
+    public static final int DELETED = 3;
+
+}
