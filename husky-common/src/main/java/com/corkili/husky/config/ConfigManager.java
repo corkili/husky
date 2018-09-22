725da5d60ae39aa814ba91152b771de1774759d2
corkili <corkili@163.com>
2018-09-22 21:34:45 +0800

    #12 修改config

--- husky-common/src/main/java/com/corkili/husky/config/ConfigManager.java
+++ husky-common/src/main/java/com/corkili/husky/config/ConfigManager.java
@@ -2,25 +2,32 @@ package com.corkili.husky.config;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import com.corkili.husky.config.loader.ConfigLoaderFactory;
+import lombok.extern.slf4j.Slf4j;
+
 import com.corkili.husky.common.Query;
 import com.corkili.husky.common.Result;
+import com.corkili.husky.config.loader.ConfigLoaderFactory;
 import com.corkili.husky.util.CheckUtils;
 import com.corkili.husky.util.IUtils;
 
+@Slf4j
 public final class ConfigManager {
 
     private static ConfigManager instance;
 
     private Config config;
     private Map<String, ConfigType> fileTypeMap;
+    private List<ConfigFile> configFiles;
     private Map<String, Long> fileLastModifiedTimeMap;
     private ScheduledExecutorService executor;
     private int updateConfigInterval;
@@ -45,31 +52,28 @@ public final class ConfigManager {
         return this.config;
     }
 
-    public Result<Void> registerConfigFile(String filename) {
+    public Result<Void> registerConfigFile(ConfigFile configFile) {
         for (ConfigType configType : ConfigType.values()) {
-            if (filename.endsWith(configType.suffix())) {
-                return registerConfigFile(filename, configType);
+            if (configFile.getFilename().endsWith(configType.suffix())) {
+                return registerConfigFile(configFile, configType);
             }
         }
         return new Result<>(false, MessageCode.FILE_TYPE_NOT_SUPPORTED,
-                IUtils.format("Type of File \"{}\" is not supported", filename), null);
+                IUtils.format("Type of File \"{}\" is not supported", configFile.getFilename()), null);
     }
 
-    public Result<Void> registerConfigFile(String filename, ConfigType configType) {
-        if (CheckUtils.hasNull(filename, configType)) {
+    public Result<Void> registerConfigFile(ConfigFile configFile, ConfigType configType) {
+        if (CheckUtils.hasNull(configFile, configType)) {
             return new Result<>(false, MessageCode.INVALID_ARGUMENT,
                     "invalid arguments: arguments is null", null);
         }
-        URL url = this.getClass().getClassLoader().getResource(filename);
-        if (url == null) {
-            return new Result<>(false, MessageCode.FILE_NOT_EXIST,
-                    IUtils.format("File \"{}\" not found", filename), null);
+        Result<Void> result = checkAndAddConfigFile(configFile, configType);
+        if (result.fail()) {
+            return result;
         }
-        fileTypeMap.put(filename, configType);
-        fileLastModifiedTimeMap.put(filename, Long.MIN_VALUE);
         updateConfig();
         return new Result<>(true, MessageCode.SUCCESS,
-                IUtils.format("File \"{}\" register successfully", filename), null);
+                IUtils.format("File \"{}\" register successfully", configFile.getFilename()), null);
     }
 
     public void shutdown() {
@@ -82,11 +86,9 @@ public final class ConfigManager {
         config = new Config();
         fileTypeMap = new ConcurrentHashMap<>();
         fileLastModifiedTimeMap = new ConcurrentHashMap<>();
-        String defaultPaConfFile = System.getProperty(ConfigConstant.SYSTEM_CONFIGURE_CONF_FILE,
-                ConfigConstant.DEFAULT_CONFIGURE_CONF_FILE);
-        registerConfigFile(defaultPaConfFile);
-        updateConfigInterval = config.getInteger(ConfigConstant.CONFIG_NAME_UPDATE_CONFIG_INTERVAL_SEC,
-                ConfigConstant.CONFIG_DEFAULT_UPDATE_CONFIG_INTERVAL_SEC);
+        configFiles = new CopyOnWriteArrayList<>();
+        updateConfigInterval = Integer.valueOf(System.getProperty(ConfigConstant.CONFIG_NAME_UPDATE_CONFIG_INTERVAL_SEC,
+                ConfigConstant.CONFIG_DEFAULT_UPDATE_CONFIG_INTERVAL_SEC));
     }
 
     private void initTask() {
@@ -99,16 +101,16 @@ public final class ConfigManager {
         executor.scheduleAtFixedRate(this::updateConfig, updateConfigInterval, updateConfigInterval, TimeUnit.SECONDS);
     }
 
-    private void updateConfig() {
+    private synchronized void updateConfig() {
         Map<String, String> configMap = new HashMap<>();
-        fileTypeMap.forEach((filename, type) -> {
-            File file = new File(filename);
-            if (file.exists() && file.lastModified() > fileLastModifiedTimeMap.get(filename)) {
-                fileLastModifiedTimeMap.put(filename, file.lastModified());
-                configMap.putAll(loadConfigFile(filename, type));
-            }
-        });
-        config.updateConfigMap(configMap);
+        if (shouldUpdate()) {
+            configFiles.forEach(configFile -> {
+                String filename = configFile.getFilename();
+                configMap.putAll(loadConfigFile(filename, fileTypeMap.get(filename)));
+            });
+            config.updateConfigMap(configMap);
+            log.debug(IUtils.format("config - {}", config));
+        }
     }
 
     private Map<String, String> loadConfigFile(String filename, ConfigType configType) {
@@ -117,5 +119,71 @@ public final class ConfigManager {
         Result<Map<String, String>> result = ConfigLoaderFactory.getConfigLoader(configType).load(query);
         return result.success() ? result.data() : new HashMap<>();
     }
+
+    private boolean shouldUpdate() {
+        boolean res = false;
+        for (String filename : fileTypeMap.keySet()) {
+            File file = null;
+            try {
+                URL url = this.getClass().getClassLoader().getResource(filename);
+                if (url != null) {
+                    file = new File(url.toURI());
+                }
+            } catch (Exception e) {
+                log.error(IUtils.stringifyError(e));
+            }
+            if (file != null && file.exists() && file.lastModified() > fileLastModifiedTimeMap.get(filename)) {
+                fileLastModifiedTimeMap.put(filename, file.lastModified());
+                res = true;
+            }
+        }
+        return res;
+    }
+
+    private Result<Void> checkAndAddConfigFile(ConfigFile configFile, ConfigType configType) {
+        if (configFile.getPriority() == ConfigFile.DEFAULT_PRIORITY) {
+            return addConfigFile(new ConfigFile(configFile.getFilename(), generatePriority()), configType);
+        } else if (isDuplicatePriority(configFile)){
+            return new Result<>(false, MessageCode.DUPLICATE_PRIORITY,
+                    IUtils.format("Priority of file \"{}\" is duplicate", configFile.getFilename()), null);
+        } else if (fileTypeMap.containsKey(configFile.getFilename())) {
+            return new Result<>(false, MessageCode.DUPLICATE_PRIORITY,
+                    IUtils.format("Filename of file \"{}\" is duplicate", configFile.getFilename()), null);
+        } else {
+            return addConfigFile(configFile, configType);
+        }
+    }
+
+    private int generatePriority() {
+        if (configFiles.isEmpty()) {
+            return 1;
+        } else {
+            return configFiles.get(configFiles.size() - 1).getPriority() + 1;
+        }
+    }
+
+    private boolean isDuplicatePriority(ConfigFile configFile) {
+        for (ConfigFile file : configFiles) {
+            if (file.getPriority() == configFile.getPriority()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Result<Void> addConfigFile(ConfigFile configFile, ConfigType configType) {
+        URL url = this.getClass().getClassLoader().getResource(configFile.getFilename());
+        if (url == null) {
+            return new Result<>(false, MessageCode.FILE_NOT_EXIST,
+                    IUtils.format("File \"{}\" not found", configFile.getFilename()), null);
+        }
+        fileTypeMap.put(configFile.getFilename(), configType);
+        fileLastModifiedTimeMap.put(configFile.getFilename(), Long.MIN_VALUE);
+        configFiles.add(configFile);
+        Collections.sort(configFiles);
+        return new Result<>(true, MessageCode.SUCCESS,
+                IUtils.format("add config file - {} successfully", configFile), null);
+    }
+    
 
 }
